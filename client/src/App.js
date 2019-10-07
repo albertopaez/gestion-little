fec1c9c5b06196abdc0095043fc3e2053cd66ba8
alberto <albertopaez92@gmail.com>
2019-10-07 14:02:29 +0200

    update-merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,23 @@
-import React, { lazy,  Suspense} from 'react';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
-import { Provider } from 'react-redux';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import Loading from './components/loading';
-import configureStore from './store'
 
 function App() {
-
-  const DefaultLayout = lazy(() => import('./container/DefaultLayout/index'))
-
-  const store = configureStore()
-
   return (
     <div className="App">
       <header className="App-header">
-        <Provider store={store}>
-        <Router>
-          <main>
-              <Suspense fallback={<Loading />}>
-                <Switch>
-                  <Route path="/" component={DefaultLayout} />
-                </Switch>
-              </Suspense>
-          </main>
-        </Router>
-        </Provider>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
